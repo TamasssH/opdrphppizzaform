ac106f46d89388bb088413ea0e44fbf81f20860d
TamasH <unconfigured@null.spigotmc.org>
2023-03-16 09:42:36 +0100

    Nieuwe font
    nieuwe font toegevoegd.

--- index.php
+++ index.php
@@ -76,9 +76,12 @@
     <head>
         <title>Pizza</title>
         <meta charset="UTF-8">
-        <meta  name="viewport" content="width=device-width,
-        initial-scale=1.0">
+        <meta  name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="style.css?t=1"> 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,300&display=swap" rel="stylesheet">
+
     </head>
     <body>
         <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
